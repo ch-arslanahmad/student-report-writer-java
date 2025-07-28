aedf251dc5c957f1fd6d23e17627436c038d8876
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-28 22:22:18 +0500

    feat: added Scanner, making 2 objects and calling getters

--- student-report-writer/Main.java
+++ student-report-writer/Main.java
@@ -1,8 +1,18 @@
 // this will be the main file
 
+// import input library: Scanner
+import java.util.Scanner;
 public class Main {
 
+
+  // main method
   public static void main(String[] args) {
+    // Making Scanner input Object
+    Scanner input = new Scanner(System.in);
+    Student student = new Student()
+    student.getName();
+    student.getAge();
+    student.gradeYr();
     
   }
 
