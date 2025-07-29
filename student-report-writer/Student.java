a93fb25783ccda364d2defd67549a9129f7c5664
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-29 15:42:53 +0500

    feat(Student.java): getters methods to get Student input

--- student-report-writer/Student.java
+++ student-report-writer/Student.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Student {
+    private String name;
+    private int age;
+    private int gradeYr;
+  
+    public String getName(Scanner input) {
+        name = input.nextLine();
+        return name;
+    }
+
+    public int getAge(Scanner input) {
+        age = input.nextInt();
+        return age;
+    }
+
+    public int getGradeYr(Scanner input) {
+        gradeYr = input.nextInt();
+        return gradeYr;
+    }
+    
     
 }
