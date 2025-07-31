a19c07d05f5b57fbd8b234f9c15f5b1168ed8b32
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-31 20:35:45 +0500

    feat(ReportWriter): added methods with on file writing/formating

--- student-report-writer/ReportWriter.java
+++ student-report-writer/ReportWriter.java
@@ -1,41 +1,78 @@
 import java.io.File;
-import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.IOException;
-import java.util.Scanner;
 public class ReportWriter {
+
+    private boolean fileExists = false;
   
     // solve the object problem
-    void writeDetails(Scanner input) {
-        Student details = new Student();
+    void writeDetails(Student student) {
+        // create an object
         try {
-            FileWriter thefile = new FileWriter("Report.txt", true);
-            thefile.write("Name: " + details.getName(input));
-            thefile.write("\tAge: " + details.getAge(input));
-            thefile.write("\tGrade Level: " + details.getGradeYr(input));
-            thefile.write("\nSubjects | Total Marks | Obtained Marks");            
-            thefile.close();
+            PrintWriter thefile = new PrintWriter("Report.txt");
+            thefile.write("Name: " + student.getName());
+            thefile.write("\tAge: " + student.getAge() + "\t");
+            thefile.write("\tGrade Level: " + student.getGradeYr() + "\n");
+                int max = student.findMaxlength();
+                thefile.printf("\n%-"+ max + "s | %-11s | %-14s | %-11s\n", "Subjects", "Total Marks", "Obtained Marks", "Percentages");
+                for(int i=0; i<student.sizeSubject(); i++) {
+                    thefile.printf("%-" + max + "s | %-11d | %-14d | %-6.2f%%\n",
+                    student.getSubject(i),
+                    student.getsubjectTotalMarks(i),
+                    student.getsubjectObtMarks(i),
+                    student.getsPercentages(i)
+                    );
+                }
+                thefile.printf("-".repeat(50) + "\n");
+                thefile.printf("%-" + max + "s | %-11d | %-14d | %6.2f%%\n", "Total",
+                student.getTotalMarks(),
+                student.getTotalObtMarks(),
+                student.getTotalPercentage());
+                thefile.close();
         } catch (IOException e) {
-            System.out.println("Error Occured");
+            System.out.println("Error Detected!");
             e.printStackTrace();
         }
 
+
     }
 
-    void fileWriter() {
-
+    void fileMaker(Student student) {
         try {
            File makeFile = new File("Report.txt");
+
            if(!makeFile.exists()) {
             makeFile.createNewFile();
-           }
+            System.out.println("File successfully created.");
+            fileExists = true;
+            
+            writeDetails(student);
+                    if(makeFile.exists() && makeFile.length() != 0) {
+                        System.out.println("Report successfully added in File.");
+                }
+                else {
+                        System.out.println("Unable to add Report.");
+                        makeFile.delete();
+                        System.out.println("Hence Deleted.....");
+                }
+            }
+           
            else {
             System.out.println("File already exists.");
+            makeFile.delete();
+            System.out.println("File Deleted.");
+            writeDetails(student);
+            System.out.println("Created newFile with the student report.");
            }
-        writeDetails(new Scanner(System.in));
         }
         catch (Exception e) {
             System.out.println("Error occured when making a file!");
         }
 
+    }
+
+    // getter for file existance
+    boolean fileState() {
+        return fileExists;
     }
 }
