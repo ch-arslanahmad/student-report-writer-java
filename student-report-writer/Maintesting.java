1d9341c904c21c36a9725be52fa588597c45de8b
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-31 20:26:38 +0500

    feat(Test): testing code with console based methods

--- student-report-writer/Maintesting.java
+++ student-report-writer/Maintesting.java
@@ -1,3 +1,5 @@
+/// this is simply testing class and is not needed for the compiling and running of the application
+
 // this was made to solve the issue of adding subjects like newline and spaces and input problem
 
 import java.util.Scanner;
@@ -5,31 +7,36 @@ import java.util.ArrayList;
 class Maintesting {
     private ArrayList<String> subject = new ArrayList<>();
 
-    void addSubject(Scanner input) {
-        System.out.println("Type '0' to exit.");
-        int n = 1;
-        while (true) {
-            System.out.print("Enter subject#" + n + ": ");
-            String a = input.nextLine();
-            if(a.equals("0")) {
-            return;
-            }
-            else {
-            subject.add(a);
-            }
-            n++;
-
-        }
-    }
-
-public static void main(String[] args) {
+    // this code that can print details in the console
+  public static void main(String[] args) {
+    // Making Scanner input Object
     Scanner input = new Scanner(System.in);
-    Maintesting student = new Maintesting();
-    
+    Student student = new Student();
     student.addSubject(input);
-    
+    student.setMarks(input);
+    input.close();
+
+    int max = student.findMaxlength();
+    System.out.printf("%-"+ max + "s | %-11s | %-14s | %-11s\n", "Subjects", "Total Marks", "Obtained Marks", "Percentages");
+    for(int i=0; i<student.sizeSubject(); i++) {
+        System.out.printf("%-" + max + "s | %-11d | %-14d | %-6.2f%%\n",
+        student.getSubject(i),
+        student.getsubjectTotalMarks(i),
+        student.getsubjectObtMarks(i),
+        student.getsPercentages(i)
+        );
+      }
+      System.out.printf("-".repeat(50) + "\n");
+      System.out.printf("%-" + max + "s | %-11d | %-14d | %6.2f%%\n", "Total",
+      student.getTotalMarks(),
+      student.getTotalObtMarks(),
+      student.getTotalPercentage());    
+
+
+  }
+  
 }
-}
+
 
 
 
