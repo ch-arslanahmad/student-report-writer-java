d78290c7f4886a5631d87d8f0e79442a09858853
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-31 21:30:11 +0500

    feat(Main): integrated Student, ReportWriter class to create Student report Result in a file

--- student-report-writer/Main.java
+++ student-report-writer/Main.java
@@ -9,20 +9,22 @@ public class Main {
   public static void main(String[] args) {
     // Making Scanner input Object
     Scanner input = new Scanner(System.in);
+    // Making Student Object
     Student student = new Student();
-    student.getName(input);
-    student.getAge(input);
-    student.getGradeYr(input);
+    // Making ReportWriter object
+    ReportWriter resultReport = new ReportWriter();
+    // calling methods to input main details
+    student.setDetails(input);
+    // calling methods to input main subject names
     student.addSubject(input);
+    // calling methods to input all marks details
     student.setMarks(input);
+
+    // create then write & format Student report in the file 
+    resultReport.fileMaker(student);
+
+    // closing the input to prevent unexpected behaviour
     input.close();
-
-    System.out.println("Total Marks: " + student.getTotalMarks());
-    System.out.println("Total Obtained Marks: " + student.getTotalObtMarks());
-
-
-    student.calculateSubjectPercentage();
-    System.out.println("Total Percentage: " + student.getTotalPercentage() + "%");
 
 
   }
