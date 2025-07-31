be4dd8cd7f7cabfcde13fd18a3847da6856968ec
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-31 12:16:31 +0500

    feat: added writeDetails & fileWriter method

--- student-report-writer/ReportWriter.java
+++ student-report-writer/ReportWriter.java
@@ -1,3 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 public class ReportWriter {
-    
+  
+    // solve the object problem
+    void writeDetails(Scanner input) {
+        Student details = new Student();
+        try {
+            Scanner writer = new Scanner(System.in);
+            FileWriter thefile = new FileWriter("Report.txt");
+            thefile.write("Name: " + details.getName(input));
+            thefile.write("\tAge: " + details.getAge(input));
+            thefile.write("\tGrade Level: " + details.getGradeYr(input));
+        } catch (IOException e) {
+            System.out.println("Error Occured");
+            e.printStackTrace();
+        }
+
+    }
+
+    void fileWriter() {
+
+        try {
+           File makeFile = new File("Report.txt");
+           if(!makeFile.exists()) {
+            makeFile.createNewFile();
+           }
+           else {
+            System.out.println("File already exists.");
+           }
+        writeDetails();
+        } catch (Exception e) {
+        }
+
+    }
 }
