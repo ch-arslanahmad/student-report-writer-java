a333b23e534ea16306f99b97b05f35afa1df3e63
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-31 10:43:54 +0500

    test(Main.java): code to test Student.java || successfull

--- student-report-writer/Main.java
+++ student-report-writer/Main.java
@@ -9,11 +9,23 @@ public class Main {
   public static void main(String[] args) {
     // Making Scanner input Object
     Scanner input = new Scanner(System.in);
-    Student student = new Student()
-    student.getName();
-    student.getAge();
-    student.gradeYr();
-    
+    Student student = new Student();
+    student.getName(input);
+    student.getAge(input);
+    student.getGradeYr(input);
+
+    student.addSubject(input);
+    student.setMarks(input);
+    input.close();
+
+    System.out.println("Total Marks: " + student.getTotalMarks());
+    System.out.println("Total Obtained Marks: " + student.getTotalObtMarks());
+
+
+    student.calculateSubjectPercentage();
+    System.out.println("Total Percentage: " + student.getTotalPercentage() + "%");
+
+
   }
 
   
