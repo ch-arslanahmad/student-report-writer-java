8473a6c459ec58c7f3a772ddcd707e4d8f8d14ef
ch-arslanahmad <ch.arslanad@gmail.com>
2025-09-27 18:52:36 +0500

    refactor(Student): improve data structure management and method organization - Replace global ArrayList dependencies with inline collections - Move input validation logic to dedicated input handling - Introduce Marks record for better data encapsulation - Add comprehensive getter/setter methods - Remove redundant calculation methods - Streamline codebase by eliminating duplicate functionality - Improve separation of concerns between data and input handling

--- student-report-writer/model/Student.java
+++ student-report-writer/model/Student.java
@@ -1,7 +1,10 @@
+
 // import Scanner: input
 import java.util.Scanner;
 // import Arraylist
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 // class
 public class Student {
@@ -11,203 +14,208 @@ public class Student {
     private int gradeYr;
     boolean isValid = true;
 
-    // creating Arraylist: subject.
-    private ArrayList<String> subject = new ArrayList<>();
-    // data type cannot be primitive, hence a wrapper used, Integer for 'int' datatype.
-    private ArrayList<Integer> subjectTotalMarks = new ArrayList<>();
+    // totals.
     private int totalMarks;
-    private ArrayList<Integer> marks = new ArrayList<>();
     private int totalObtMarks;
-    private ArrayList<Float> marksPercentage = new ArrayList<>();
     private float totalPercentage;
-  
 
-    void setDetails(Scanner input) {
-        // get name
-        System.out.print("Enter your name: ");
-        name = input.nextLine();       
-        // get age
-        System.out.print("Enter your age: ");
-        age = input.nextInt();
-        // get grade year
-        System.out.print("Enter your grade year: ");
-        gradeYr = input.nextInt();
+    // a custom record for total and obtained marks of a subject
+    public record Marks(int total, int obtained) {
+    }
 
-        // this is to consume the newline character left by input
-        input.nextLine();
+    // default Constructor
+    public Student(String name, int age, int gradeYr) {
+        this.name = name;
+        this.age = age;
+        this.gradeYr = gradeYr;
+    }
+
+    // empty constructor
+
+    public Student() {
 
     }
+
     // Name getter
     public String getName() {
         return name;
     }
-    
+
     // student age getter
     public int getAge() {
         return age;
     }
-    
+
     // student grade getter
     public int getGradeYr() {
         return gradeYr;
 
     }
 
+    // SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGradeYr(int gradeYr) {
+        this.gradeYr = gradeYr;
+    }
 
     // add Subject
-    void addSubject(Scanner input) {
+    public ArrayList<String> addSubject(Scanner input) {
+        ArrayList<String> subject = new ArrayList<>();
+
         System.out.println("Type '0' to exit.");
         int n = 1;
         while (true) {
             System.out.print("Enter subject#" + n + ": ");
             String a = input.nextLine();
-            if(a.equals("0")) {
+            if (a.equals("0")) {
                 break;
-            }
-            else {
+            } else {
                 subject.add(a);
             }
             n++;
-
         }
+        return subject;
+
     }
 
     // find maxlength for dynamic output
-    int findMaxlength() {
+    public int findMaxlength(ArrayList<String> subject) {
         int max = subject.get(0).length();
-        for(int i=1; i<subject.size(); i++) {
-            if(max<subject.get(i).length()) {
+        for (int i = 1; i < subject.size(); i++) {
+            if (max < subject.get(i).length()) {
                 max = subject.get(i).length();
             }
         }
-        if(max<10)
+        if (max < 10)
             max = 10;
         return max;
     }
+
     // getter for subject
-    String getSubject(int i) {
+    String getSubject(ArrayList<String> subject, int i) {
         return subject.get(i);
     }
+
     // 'size' of subject arraylist
-    int sizeSubject() {
+    int sizeSubject(ArrayList<String> subject) {
         return subject.size();
     }
 
+    // ? assumes multiple subjects exist - (Subject) TOTAL MARKS
+    public int setSubjectTotalMarks(String subject, Scanner input) {
+        while (true) {
+            System.out.print("Enter total marks of " + subject + ": ");
+            if (input.hasNextInt()) { // using it for validation
+                int subjectTotalMarks = input.nextInt();
+                input.nextLine(); // consume newline (left by enter)
+                if (subjectTotalMarks > 0) {
+                    return subjectTotalMarks;
+                } else {
+                    System.out.println("Total marks must be greater than zero!");
+                }
+            } else {
+                System.out.println("Please enter a valid integer.");
+                input.nextLine(); // remove invalid input
+            }
+        }
+    }
+
+    // assumes multiple subjects exist // ... (Subject) OBT MARKS
+    public int setSubjectMarks(String subject, int subjectMarks, Scanner input) {
+        while (true) {
+            System.out.print("Enter obtained marks of " + subject + ": ");
+            if (input.hasNextInt()) { // using it for validation
+                int obtMarks = input.nextInt();
+                input.nextLine(); // consume newline (left by ENTER)
+                if (obtMarks < 0) {
+                    System.out.println("Obtained marks cannot be negative.");
+                } else if (obtMarks > subjectMarks) {
+                    System.out.println("Obtained marks cannot be greater than total marks (" + subjectMarks + ").");
+                } else {
+                    return obtMarks;
+                }
+            } else {
+                System.out.println("Please enter a valid integer.");
+                input.nextLine(); // remove invalid input
+            }
+        }
+    }
+
     // adds Marks for each subject
-    void setMarks(Scanner input) {
-        // runs if subject is not 0.  
-        if(sizeSubject() != 0) {
-            for(int i=0; i<sizeSubject(); i++) {
-            System.out.print("Enter total marks of " + subject.get(i) + ": ");
-            // used add() method of ArrayList for Total marks of subject: as add adds value even if list is empty
-            int subjectMarks = input.nextInt();
-            if(subjectMarks != 0) {
-                subjectTotalMarks.add(subjectMarks);
-            }
-            else {
-                System.out.println("Total marks cannot be zero!");
-            }
-            System.out.print("Enter obtained marks of " + subject.get(i) + ": ");
-            // again add() method for obtained marks
-            marks.add(input.nextInt());
-            }
+    public Map<String, Marks> setAllMarks(ArrayList<String> subjects, Scanner input) {
+
+        Map<String, Marks> marks = new HashMap<>();
+
+        for (String subject : subjects) {
+            int total = setSubjectTotalMarks(subject, input);
+            int obtained = setSubjectMarks(subject, total, input);
+
+            marks.put(subject, new Marks(total, obtained));
         }
-        else {
-            System.out.print("No subjects were added!");
-        }
-        
-        // this is to consume the newline character left by input
-        input.nextLine();
-    }
-// anyway keep in mind: that subject and total & obtained marks will be equal obviosly, hence add a validation for it, if needed.
-
-    // getter for subject total marks
-    int getsubjectTotalMarks(int i) {
-        return subjectTotalMarks.get(i);
-    }
-
-    // 'size' of marks arraylist
-    int sizesubjectMarks() {
-        return subjectTotalMarks.size();
-    }
-
-    // getter for subject obtained marks
-    int getsubjectObtMarks(int i) {
-        return marks.get(i);
-    }
-
-
-    // 'size' of obtained marks arraylist
-    int sizeObtMarks() {
-        return marks.size();
+        return marks;
     }
 
     // fixed logic: Total Marks method
-    int getTotalMarks() {
+    int getTotalMarks(ArrayList<Integer> subjectTotalMarks) {
         // totalMarks
-        for(int i = 0; i<subjectTotalMarks.size(); i++) {
-        totalMarks += subjectTotalMarks.get(i);
+        for (int i = 0; i < subjectTotalMarks.size(); i++) {
+            totalMarks += subjectTotalMarks.get(i);
         }
         return totalMarks;
     }
 
     // total obtained-marks
-    int getTotalObtMarks() {
-        for(int i = 0; i<marks.size(); i++) {
-        totalObtMarks += marks.get(i);
+    int getTotalObtMarks(ArrayList<Integer> marks) {
+        for (int i = 0; i < marks.size(); i++) {
+            totalObtMarks += marks.get(i);
         }
         return totalObtMarks;
     }
 
     // find percentage
     float percentage(int num, int total) {
-        float percentage = (float) num/ (float) total * 100f;
-        return percentage;
+        return (float) num / (float) total * 100f;
     }
-    
+
     // find percentage of specific subject
-    void calculateSubjectPercentage() {
+    ArrayList<Float> calculateSubjectPercentage(ArrayList<String> subjects, ArrayList<Integer> subjectTotalMarks,
+            ArrayList<Integer> marks) {
 
-        if(subject.size() != 0) {
-            for(int i=0; i<subject.size(); i++) {
-                marksPercentage.add(percentage(marks.get(i),subjectTotalMarks.get(i)));
-            }
+        ArrayList<Float> subjectPercentages = new ArrayList<>();
+
+        for (int i = 0; i < subjects.size(); i++) {
+            float perc = percentage(marks.get(i), subjectTotalMarks.get(i));
+            subjectPercentages.add(perc);
         }
+        return subjectPercentages;
+
     }
-    
-
-    // getter for subject obtained marks
-    float getsPercentages(int i) {
-        calculateSubjectPercentage();
-        return marksPercentage.get(i);
-    }
-
-
-    // 'size' of obtained marks arraylist
-    int sizePercentages() {
-        return marksPercentage.size();
-    }
-
 
     // find Percentage as a whole
-/*
-simply it is an average of percentages:
-so optimally average formula should be used.
-avg = sum of all values / total number of values
- */
-void calculateTotalPercentage() {
+    /*
+     * simply it is an average of percentages:
+     * so optimally average formula should be used.
+     * avg = sum of all values / total number of values
+     */
+    void calculateTotalPercentage(ArrayList<Float> marksPercentage) {
         // to find the maximum number to be the divisor
         try {
-        if(marksPercentage.size() > 0) {
-            totalPercentage =  0f;
-                for(int i = 0; i<marksPercentage.size(); i++) {
+            if (marksPercentage.size() > 0) {
+                totalPercentage = 0f;
+                for (int i = 0; i < marksPercentage.size(); i++) {
                     totalPercentage += marksPercentage.get(i);
                 }
-        totalPercentage = totalPercentage / marksPercentage.size();
-        }
-        else {
-            throw new Exception("You need subjects to calculate percentages of.");
-        }         
+                totalPercentage = totalPercentage / marksPercentage.size();
+            } else {
+                throw new Exception("You need subjects to calculate percentages of.");
+            }
         } catch (Exception e) {
             System.out.println("Error occured");
             e.printStackTrace();
@@ -215,11 +223,9 @@ void calculateTotalPercentage() {
 
     }
 
-float getTotalPercentage() {
-    calculateTotalPercentage();
-    return totalPercentage;
-}
+    float getTotalPercentage(ArrayList<Float> marksPercentage) {
+        calculateTotalPercentage(marksPercentage);
+        return totalPercentage;
+    }
 
-
-    
 }
